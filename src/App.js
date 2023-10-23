db25a3c1afe16c21d4875f2e30891a846d1c779b
Guillermo Santolaya <guillermosantolaya.93@gmail.com>
2023-10-23 17:58:31 -0300

    modificando url base

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import axios from "axios"
 import { AppRouter } from './router/AppRouter'
 
-axios.defaults.baseURL = process.env.LAZURDITA_APP_API_URL || "LAZURDITA_APP_API_URL=https://node-la-zurdita-app-api.onrender.com/api"
+axios.defaults.baseURL = process.env.LAZURDITA_APP_API_URL || "https://node-la-zurdita-app-api.onrender.com/api"
 
 function App() {
   return (
