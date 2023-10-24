a79ea5b033b6ccbc8d65d423d042a4b0b3c4b79c
Guillermo Santolaya <guillermosantolaya.93@gmail.com>
2023-10-24 00:47:41 -0300

    agregando .env a gitignore

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css'
 import axios from "axios"
 import { AppRouter } from './router/AppRouter'
 
-axios.defaults.baseURL = process.env.REACT_APP_LAZURDITA_API_URL
-
+axios.defaults.baseURL = process.env.REACT_APP_LAZURDITA_API_URL || "https://node-la-zurdita-app-api.onrender.com/api"
+console.log(axios.defaults.baseURL)
 function App() {
   return (
 <div className="App">
