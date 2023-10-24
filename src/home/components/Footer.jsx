e04d4783ee25f32bc8b90034c1f263ba986be782
Guillermo Santolaya <guillermosantolaya.93@gmail.com>
2023-10-24 19:41:49 -0300

    corrigiendo responsive del footer

--- src/home/components/Footer.jsx
+++ src/home/components/Footer.jsx
@@ -4,14 +4,10 @@ import logoNavbar from '../components/img/Imagen_de_WhatsApp_2023-10-02_a_las_15
 
 export function Footer() {
   return (
-    <div className="d-flex footerContainer px-4 px-md-5">
-      <div id='contactContainer' className="col-12 col-md-6 col-lg-4 my-2 ">
-      </div>
+    <div className="d-flex footerContainer px-4 px-md-5 justify-content-center">
       <div id='logoContainer' className="col-12 col-md-6 col-lg-4 d-flex row align-items-center justify-content-center">
       <img src={logoNavbar} className="appLogoFooter m-1" alt="logo" />
       <span><i>Todos los derechos reservados &copy; 2023 La Zurdita</i></span>
-      </div>
-      <div id='socialMediaContainer' className="col-12 col-md-6 col-lg-4 d-flex align-items-center justify-content-end ">
       </div>
     </div >
   )
